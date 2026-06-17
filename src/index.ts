78f3a9a041768827aab571aba61173d60a9bf262
nicoeltes <nicolaseltes@gmail.com>
2026-06-17 16:02:54 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 import { preguntar } from './lib/consola.ts';
 
+
+
+
 function repetirLetra(letra: string, cantidad: number): string {
-  // COMPLETAR
-}
+  let texto:string="";
+let contador:number=0
+while(contador<cantidad){
+texto= texto + letra;
+}}
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
 
 // COMPLETAR
+  while (tamaño>0){
+console.log(repetirLetra(caracter,tamaño))
+tamaño=tamaño-1
+
+
+  }
+  
